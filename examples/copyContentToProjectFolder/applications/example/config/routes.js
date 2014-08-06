483c7a6a822b39e0408d37f0721c2605c103eb39
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2014-08-06 03:47:23 +0300

    Added url-rewriting example to config

--- examples/copyContentToProjectFolder/applications/example/config/routes.js
+++ examples/copyContentToProjectFolder/applications/example/config/routes.js
@@ -1,6 +1,7 @@
 // URL rewriting and request forwarding rules
 
 module.exports = [
+/*
 	{
 		url:      "/api/(one|two)/(.*)",        // parse url using "()" as parameters, "|" as alternatives and ".*"
 		rewrite:  "/example/[1].json?par1=[2]"  // rewrite url using [1]..[N] array (optional parameter)
@@ -11,5 +12,11 @@ module.exports = [
 		host:     "example.com",                   // forward requests to specified host and port
 		port:     80,
 		slowTime: "1s"
+	},
+	{
+		escaping: false,              // use full regexp syntax, no escaping for "?" and "."
+		url:      "^/(?!client)(.*)",
+		rewrite:  "/client/[1]"
 	}
+*/
 ]
