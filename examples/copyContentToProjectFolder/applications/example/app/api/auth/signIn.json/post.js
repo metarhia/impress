b674bd1abdb5fe7d179b8b9527f493ce74716090
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2014-08-31 00:58:07 +0300

    Refactored impress.security
    * Refactored .register() .signIn(), .signOut()
    * sessionCreated and sessionModified moved to Client class from session
    hash
    * Examples fixed

--- examples/copyContentToProjectFolder/applications/example/app/api/auth/signIn.json/post.js
+++ examples/copyContentToProjectFolder/applications/example/app/api/auth/signIn.json/post.js
@@ -1,6 +1,6 @@
 ﻿module.exports = function(client, callback) {
 
-	security.signIn(client, function(isSuccess) {
+	security.signIn(client, client.fields.Login, client.fields.Password, function(isSuccess) {
 		if (isSuccess) client.context.data = { Result: "Ok" };
 		else client.context.data = { Result: "Error" };
 		if (client.fields.loginForm) client.redirect("/");
