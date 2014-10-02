3b3d7d84681b9ade72511e609f2c5531270fa7f9
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2014-10-03 01:32:06 +0300

    Version 0.1.109
    * Minor improves in service and deploy scripts

--- bin/install.sh
+++ bin/install.sh
@@ -2,8 +2,8 @@
 cp /impress/node_modules/impress/bin/impress /etc/init.d
 chmod +x /etc/init.d/impress
 if [ -f /etc/debian_version ]; then
-	sudo update-rc.d impress defaults
+  sudo update-rc.d impress defaults
 elif [ -f /etc/redhat-release ]; then
-	chkconfig --add impress
-	chkconfig impress on
+  chkconfig --add impress
+  chkconfig impress on
 fi
--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -3,7 +3,7 @@ service impress stop
 if [ -f /etc/debian_version ]; then
 	sudo update-rc.d impress disable
 elif [ -f /etc/redhat-release ]; then
-	chkconfig impress off
-	chkconfig --del impress
-	rm /etc/init.d/impress
+  chkconfig impress off
+  chkconfig --del impress
+  rm /etc/init.d/impress
 fi
