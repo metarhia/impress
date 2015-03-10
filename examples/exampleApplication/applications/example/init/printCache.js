bee6b8a59ca0f01c95d00b77ddb1c900e99501e7
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2015-03-10 05:13:28 +0200

    Fixed static cache

--- examples/exampleApplication/applications/example/init/printCache.js
+++ examples/exampleApplication/applications/example/init/printCache.js
@@ -1,17 +1,17 @@
 ﻿/*if (api.cluster.isWorker) {
   setInterval(function() {
     console.dir({
-      //cache: Object.keys(application.cache),
-      templates: Object.keys(application.cache.templates),
-      files: application.cache.files,
-      folders: application.cache.folders,
-      scripts: Object.keys(application.cache.scripts),
-      watchers: Object.keys(application.cache.watchers),
-      //timer: Object.keys(application.cache.timer),
-      updates: Object.keys(application.cache.updates),
+      //cache: JSON.stringify(Object.keys(application.cache)),
+      //templates: Object.keys(application.cache.templates),
+      //files: application.cache.files,
+      //folders: application.cache.folders,
+      //scripts: Object.keys(application.cache.scripts),
+      //watchers: Object.keys(application.cache.watchers),
+      //-timer: Object.keys(application.cache.timer),
+      //-updates: Object.keys(application.cache.updates),
       static: Object.keys(application.cache.static),
-      pages: Object.keys(application.cache.pages),
-      //size: Object.keys(application.cache.size)
+      //-pages: Object.keys(application.cache.pages),
+      //-size: Object.keys(application.cache.size)
     });
   }, 5000);
 }*/
