56779d83f8b2d44c8ee926c691464db83d2c6b0e
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2015-03-11 01:56:46 +0200

    Test added

---
+++ examples/exampleApplication/applications/example/app/examples/memory/randomDataSet.json/get.js
@@ -0,0 +1,42 @@
+﻿module.exports = function(client, callback) {
+
+  var names = [
+    'Plato', 'Zeno', 'Persaeus', 'Aristo', 'Apollophanes', 'Eudromus', 'Crates', 'Diogenes', 'Zenodotus', 'Basilides',
+    'Antipater', 'Apollodorus', 'Archedemus', 'Panaetius', 'Boethus', 'Polemon', 'Marcus', 'Heraclides', 'Dardanus',
+    'Mnesarchus', 'Stilo', 'Dionysius', 'Quintus', 'Hecato', 'Diotimus', 'Posidonius', 'Crinis', 'Diodotus', 'Jason'
+  ];
+
+  var cities = [
+    'Sidon', 'Rhodes', 'Chios', 'Antioch', 'Carthage', 'Assos', 'Cyrene', 'Amphipolis', 'Soli', 'Tarsus', 'Mallus',
+    'Babylon', 'Seleucia', 'Athens', 'Cyrene', 'Apamea', 'Cordylion', 'Tyre', 'Nysa', 'Alexandria', 'Hierapolis'
+  ];
+
+  var schools = [
+    'Milesian', 'Xenophanes', 'Pythagoreanism', 'Heraclitus', 'Eleatic philosophy', 'Pluralism', 'Atomism',
+    'Sophistry', 'Socrates', 'Plato', 'Aristotle', 'Hellenistic'
+  ];
+
+  function randomItem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  function random(min, max) {
+    return min + Math.floor(Math.random() * (max - min + 1));
+  }
+
+  var res = [];
+
+  for (var i = 0; i < 10; i++) {
+    var obj = {
+      name: randomItem(names),
+      city: randomItem(cities),
+      birth: random(1, 500) + ' ' + randomItem([ 'BC', 'AD' ]),
+      age: random(25, 90),
+      school: randomItem(schools)
+    };
+    res.push(obj);
+  }
+
+  callback(res);
+
+};
