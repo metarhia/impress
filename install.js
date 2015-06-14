4717635f1d11be67d83f357e294de11ddecce17f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2015-06-14 16:34:18 +0300

    Version 0.1.282
    Debug installation

--- install.js
+++ install.js
@@ -14,18 +14,17 @@ ncp.limit = 16;
 
 var current = path.dirname(__filename.replace(/\\/g, '/')),
     parent = path.basename(path.dirname(current)),
-    destination = path.dirname(path.dirname(current)) + '/',
-    source = current + '/examples/exampleApplication/',
+    destination = path.dirname(path.dirname(current)),
+    source = current + '/examples/exampleApplication',
     exists = false;
 
 // Execute shell command displaying output and possible errors
 //
 function execute(cmd, callback) {
-  console.log('exec(' + cmd + ')');
   exec(cmd, function(error, stdout, stderr) {
-    console.log('stdout:' + stdout);
+    console.log(stdout);
     if (error) console.log('error:' + error);
-    if (stderr) console.log('stderr:' + );
+    if (stderr) console.log('stderr:' + stderr);
     if (callback) callback();
   });
 }
@@ -35,9 +34,7 @@ function execute(cmd, callback) {
 function installCLI() {
   execute('npm install impress-cli -g', function() {
     execute('impress path ' + destination, function() {
-      execute('impress autostart on', function() {
-        console.log('Done!');
-      });
+      execute('impress autostart on');
     });
   });
 }
@@ -48,7 +45,7 @@ if (parent !== 'node_modules') {
 }
 
 async.each(['server.js', 'config', 'applications'], function(file, callback) {
-  fs.exists(destination + file, function(fileExists) {
+  fs.exists(destination + '/' + file, function(fileExists) {
     exists = exists || fileExists;
     callback();
   });
@@ -56,12 +53,12 @@ async.each(['server.js', 'config', 'applications'], function(file, callback) {
   if (exists) console.log('Impress Application Server'.bold.green + ' is already installed and configured in this folder.');
   else {
     console.log('Installing Impress Application Server...'.bold.green);
-    fs.createReadStream(source + 'server.js').pipe(fs.createWriteStream(destination+'server.js'));
+    fs.createReadStream(source + '/server.js').pipe(fs.createWriteStream(destination + '/server.js'));
     var shellScript = 'server.' + (isWin ? 'cmd' : 'sh');
-    fs.createReadStream(source + shellScript).pipe(fs.createWriteStream(destination+shellScript));
-    ncp(source + 'config', destination + 'config', { clobber: false }, function (err) {
+    fs.createReadStream(source + '/' + shellScript).pipe(fs.createWriteStream(destination + '/' + shellScript));
+    ncp(source + '/config', destination + '/config', { clobber: false }, function (err) {
       if (err) console.error(err);
-      ncp(source + 'applications', destination + 'applications', { clobber: false }, function (err) {
+      ncp(source + '/applications', destination + '/applications', { clobber: false }, function (err) {
         if (err) console.error(err);
         installCLI();
       });
