d978150aa4e7305ea076c9d37ba123ca52a20273
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2015-06-14 04:21:16 +0300

    0.1.277
    * Move /bin/* to impress-cli, issue #313

--- install.js
+++ install.js
@@ -14,16 +14,15 @@ ncp.limit = 16;
 
 var current = path.dirname(__filename.replace(/\\/g, '/')),
     parent = path.basename(path.dirname(current)),
-    destination = path.dirname(path.dirname(current))+'/',
-    source = current+'/examples/exampleApplication/',
+    destination = path.dirname(path.dirname(current)) + '/',
+    source = current + '/examples/exampleApplication/',
     exists = false;
 
-function installService() {
-  exec('chmod +x ./bin/install.sh', function() {
-    exec('chmod +x ./bin/uninstall.sh');
-    exec('chmod +x ./bin/impress');
-    exec('chmod +x ./server.sh');
-    exec('./bin/install.sh');
+function installCLI() {
+  exec('npm install impress-cli -g', function() {
+    exec('impress path ' + current, function() {
+      exec('impress autostart on');
+    });
   });
 }
 
@@ -33,38 +32,24 @@ if (parent !== 'node_modules') {
 }
 
 async.each(['server.js', 'config', 'applications'], function(file, callback) {
-  fs.exists(destination+file, function(fileExists) {
+  fs.exists(destination + file, function(fileExists) {
     exists = exists || fileExists;
     callback();
   });
 }, function() {
   if (exists) {
-    console.log('Impress Application Server'.bold.green+' is already installed and configured in this folder.');
-    if (destination === '/impress/') {
-      console.log('Refreshing service scripts.');
-      installService();
-    }
+    console.log('Impress Application Server'.bold.green + ' is already installed and configured in this folder.');
+    installCLI();
   } else {
     console.log('Installing Impress Application Server...'.bold.green);
-    fs.createReadStream(source+'server.js').pipe(fs.createWriteStream(destination+'server.js'));
+    fs.createReadStream(source + 'server.js').pipe(fs.createWriteStream(destination+'server.js'));
     var shellScript = 'server.' + (isWin ? 'cmd' : 'sh');
-    fs.createReadStream(source+shellScript).pipe(fs.createWriteStream(destination+shellScript));
-    ncp(source+'config', destination+'config', { clobber: false }, function (err) {
+    fs.createReadStream(source + shellScript).pipe(fs.createWriteStream(destination+shellScript));
+    ncp(source + 'config', destination + 'config', { clobber: false }, function (err) {
       if (err) console.error(err);
-      ncp(source+'applications', destination+'applications', { clobber: false }, function (err) {
+      ncp(source + 'applications', destination + 'applications', { clobber: false }, function (err) {
         if (err) console.error(err);
-        if (!isWin) {
-          if (destination === '/impress/') {
-            console.log('Installing Impress Application Server as a service.');
-            console.log('  Usage: service impress start|stop|restart|status');
-            installService();
-          } else {
-            console.log(
-              'To install Impress Application Server as a service to start automatically during the system startup you need to create directory '+
-              '/impress'.bold.green+' and run '+'npm install impress'.bold.green+' in this directory.'
-            );
-          }
-        }
+        installCLI();
       });
     });
   }
