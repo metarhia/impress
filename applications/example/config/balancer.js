0e6efeda1680eb34230fa617d6449e6c489339a5
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2015-10-06 23:03:26 +0300

    Client-side balancer config, issue #434

---
+++ applications/example/config/balancer.js
@@ -0,0 +1,15 @@
+// Application specific configuration
+// You can place js file in this directory with your own structure and it will be loaded as an application config section
+
+module.exports = {
+
+  servers: {
+    C0: {
+      ssl: false,
+      host: '127.0.0.1',
+      ports: [80]
+      // ports: [80, 81, 82, 83]
+    }
+  }
+
+};
--- applications/example/server/api/application/balancer.json/get.js
+++ applications/example/server/api/application/balancer.json/get.js
@@ -1,11 +1,5 @@
 ﻿module.exports = function(client, callback) {
   callback({
-    servers: {
-      S0: {
-        ssl: false,
-        host: '127.0.0.1',
-        ports: [80, 81, 82, 83]
-      }
-    }
+    servers: application.config.balancer.servers
   });
 };
