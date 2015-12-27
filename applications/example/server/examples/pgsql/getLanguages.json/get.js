d8a05bd2b2d2b34b1cd6c696771b9c1e6289a0c9
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2015-12-27 02:24:00 +0200

    Small fix in examples

--- applications/example/server/examples/pgsql/getLanguages.json/get.js
+++ applications/example/server/examples/pgsql/getLanguages.json/get.js
@@ -1,5 +1,6 @@
 module.exports = function(client, callback) {
-  aliasNamePg.query('select * from SystemLanguage where LanguageId<$1', ['10'], function(err, result) {
+  var select = 'select * from SystemLanguage where LanguageId < $1';
+  aliasNamePg.query(select, ['10'], function(err, result) {
     callback({ rows: result.rows, fields: result.fields });
   });
 };
