679acc5b8e61880267638bf1d03a5431556d75ab
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2016-01-03 23:38:53 +0200

    Config structure changes, issue #474

--- config/scale.js
+++ config/scale.js
@@ -1,44 +1,69 @@
 // Cloud and health configuration
 
 module.exports = {
+
   // check:      'http://127.0.0.1/', // if we can get this page it means that another copy is running
 
-  cloud:      'PrivateCloud',    // cloud name
-  transport:  'tcp',             // RPC transport: tcp, ipc, zmq
+  cloud: 'PrivateCloud', // cloud name
+  transport: 'tcp', // RPC transport: tcp, ipc, zmq
 
-  instance:   'controller', // cloud instance type: standalone, controller, server
-  //instance:   'server',
-  //instance:   'standalone',
+  rpcPort: 250, // cloud controller tcp port for TCP and ZMQ
+  subPort: 251, // bublisher/subscriber port for ZMQ
 
-  host:       '127.0.0.1',       // cloud controller ip or host name
-  rpcPort:     250,              // cloud controller tcp port for TCP and ZMQ
-  subPort:     251,              // bublisher/subscriber port for ZMQ
+  cookie: 'node', // Cookie name for loadbalancing (cookie value will be 'S1'+'N1')
+  health: '5m', // health monitoring interval '5s'
+  nagle: false, // Nagle algorithm
+  gcInterval: 0, // garbage collector interval '1h' - 1 hour, '10m' - 10 minutes
+  watchInterval: '2s', // combine wached file system events if interval less then specified
 
-  cluster:    'S1',              // Cluster name to identify it in loadbalancing infrastructure
-  cookie:     'node',            // Cookie name for loadbalancing (cookie value will be 'C1'+'N1')
-  strategy:   'bundle',
-    // 'single'  - one process (no master and workers)
-    // 'bundle'  - multiple processes, one master and different workers for each server (master should not listen ports)
-    // 'cluster' - multiple processes, one master and identical workers with no sticky (master should listen ports)
-    // 'sticky'  - multiple processes, one master and workers with sticky by IP (master should listen ports)
+  slowTime: '1s', // request processing time to put it to slow log
+  timeout: '30s', // critical request processing to return timeout error
+  keepAlive: '5s', // keep alive timeout
 
-  workers: 1, // api.os.cpus().length, // worker count, e.g. api.os.cpus().length-1 or just number
+  ssl: {
+    key: 'example.key',
+    cert: 'example.cer'
+  },
 
-  waf: { // Web Application Firewall config
-    enabled: false,
-    limits: { // limit concurent connection count
-      ip:   20,  // per client ip
-      sid:  10,  // per user session
-      host: 100, // per host name
-      url:  50,  // per url
-      app:  200, // per application
-      srv:  500  // per server port
+  servers: {
+    S1: {
+      instance: 'controller',
+      strategy: 'bundle',
+      services: {
+        http: [80, 4001, 4002],
+        static: [81],
+        ws: [4001, 4002],
+        rpc: [4001, 4002],
+      }
+    },
+    S2: {
+      instance: 'server', // cloud instance type: standalone, controller, server
+      strategy: 'bundle',
+      // single - one process (no master and workers)
+      // bundle - multiple processes, one master and different workers for each server (master should not listen ports)
+      // cluster - multiple processes, one master and identical workers with no sticky (master should listen ports)
+      // sticky - multiple processes, one master and workers with sticky by IP (master should listen ports)
+      workers: 1, // worker count, e.g. api.os.cpus().length-1 or just number
+      addresses: ['10.0.0.10'], // IP list or no value for all
+      applications: ['example'], // applications list or no value for all
+      services: {
+        http: [80, 81, 82, 83],
+        https: [443]
+      }
     }
   },
 
-  health:        '5m',   // health monitoring interval '5s'
-  nagle:         false,  // Nagle algorithm
-  gcInterval:    0,      // garbage collector interval '1h' - 1 hour, '10m' - 10 minutes
-  watchInterval: '2s'    // combine wached file system events if interval less then specified
+  // Web Application Firewall config
+  waf: {
+    enabled: false,
+    limits: { // limit concurent connection count
+      ip: 20,  // per client ip
+      sid: 10,  // per user session
+      host: 100, // per host name
+      url: 50,  // per url
+      app: 200, // per application
+      srv: 500  // per server port
+    }
+  },
 
 };
--- config/scale.test.js
+++ config/scale.test.js
@@ -1,24 +1,33 @@
 module.exports = {
-  cloud:      'PrivateCloud',
-  transport:  'tcp',
-  instance:   'standalone',
 
-  host:       '127.0.0.1',
-  rpcPort:     8250,
-  subPort:     8251,
+  cloud: 'PrivateCloud',
+  transport: 'tcp',
 
-  cluster:    'C1',
-  cookie:     'node',
-  strategy:   'single',
-  workers:    1,
+  rpcPort: 8250,
+  subPort: 8251,
+
+  cookie: 'node',
+  health: '5m',
+  nagle: false,
+  gcInterval: 0,
+  watchInterval: '2s',
+
+  slowTime: '5s',
+  timeout: '30s',
+  keepAlive: '5s',
+
+  servers: {
+    S1: {
+      instance: 'standalone',
+      strategy: 'single',
+      services: {
+        http: [80]
+      }
+    },
+  },
 
   waf: {
     enabled: false
-  },
-
-  health:     '5s',
-  nagle:      false,
-  gcInterval: 0,
-  watchInterval: '2s'
+  }
 
 };
