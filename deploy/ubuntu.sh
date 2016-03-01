f9754317d2024d242c5ae397a587827efddc9726
johnbizokk <7hits@mail.ru>
2016-03-02 01:50:00 +0200

    Deploy scripts fixing Added support for Debian 8.3 and Ubuntu 15.10

--- deploy/ubuntu.sh
+++ deploy/ubuntu.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+RELEASE=$(lsb_release -sc)
 sudo apt-get -y update
-sudo apt-get -y install wget mc
+sudo apt-get -y install wget mc curl
 sudo apt-get -y install build-essential openssl libssl-dev pkg-config python
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get install -y nodejs
 cd ~
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+if [ $RELEASE = 'wily' ]; then
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
+else
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
+fi
 sudo apt-get -y update
 sudo apt-get -y install mongodb-org
 sudo service mongod start
