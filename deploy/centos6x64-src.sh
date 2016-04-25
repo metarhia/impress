f4b42dd69d9307f52cb23b65b121ca30cbb042e2
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2016-04-26 00:04:27 +0300

    Version 0.1.416
    * Changed deploy scripts and Travis CI testing cases for node.js 5.11.0

--- deploy/centos6x64-src.sh
+++ deploy/centos6x64-src.sh
@@ -7,15 +7,15 @@ rpm --import http://ftp.mirrorservice.org/sites/ftp.scientificlinux.org/linux/sc
 yum -y install devtoolset-3
 source scl_source enable devtoolset-3
 cd /usr/src
-wget http://nodejs.org/dist/v5.10.1/node-v5.10.1.tar.gz
-tar zxf node-v5.10.1.tar.gz
-rm -f ./node-v5.10.1.tar.gz
-cd node-v5.10.1
+wget http://nodejs.org/dist/v5.11.0/node-v5.11.0.tar.gz
+tar zxf node-v5.11.0.tar.gz
+rm -f ./node-v5.11.0.tar.gz
+cd node-v5.11.0
 ./configure
 make
 make install
 cd ~
-rm -rf /usr/src/node-v5.10.1
+rm -rf /usr/src/node-v5.11.0
 ln -s /usr/local/bin/node /bin
 ln -s /usr/local/bin/npm /bin
 cat >/etc/yum.repos.d/mongodb.repo <<EOL
