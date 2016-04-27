bc4bca3a2b210941af2d019592f25c6e8a5f5562
Alexey Orlenko <eaglexrlnk@gmail.com>
2016-04-27 13:11:58 +0300

    Add api.jstps.interprete test and fix a bug in this function

--- tests/unittests/api.jstp.test.js
+++ tests/unittests/api.jstp.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var jsrsObject = {
+var record = {
   name: 'Marcus Aurelius',
   passport: 'AE127095',
   birth: {
@@ -22,12 +22,12 @@ var jsrsObject = {
   }
 };
 
-var jsrsString = '{name:\'Marcus Aurelius\',passport:\'AE127095\',birth:' +
+var recordString = '{name:\'Marcus Aurelius\',passport:\'AE127095\',birth:' +
   '{date:\'1990-02-15\',place:\'Rome\'},contacts:{email:\'marcus@aurelius.' +
   'it\',phone:\'+380505551234\',address:{country:\'Ukraine\',city:\'Kiev\',' +
   'zip:\'03056\',street:\'Pobedy\',building:\'37\',floor:\'1\',room:\'158\'}}}';
 
-var jsrsWhitespacedString = '{\n' +
+var recordWhitespacedString = '{\n' +
   '  name: \'Marcus Aurelius\',\n' +
   '  passport: \'AE127095\',\n' +
   '  birth: {\n' +
@@ -49,12 +49,41 @@ var jsrsWhitespacedString = '{\n' +
   '  }\n' +
   '}';
 
+var object = {
+  name: ['Marcus', 'Aurelius'].join(' '),
+  passport: 'AE' + '127095',
+  birth: {
+    date: new Date('1990-02-15'),
+    place: 'Rome'
+  },
+  age: function() {
+    var difference = new Date() - birth.date;
+    return Math.floor(difference / 31536000000);
+  }
+};
+
+var objectStringWithExpressions = '{\n' +
+  '  name: [\'Marcus\', \'Aurelius\'].join(\' \'),\n' +
+  '  passport: \'AE\' + \'127095\',\n' +
+  '  birth: {\n' +
+  '    date: new Date(\'1990-02-15\'),\n' +
+  '    place: \'Rome\'\n' +
+  '  },\n' +
+  '  age: function() {\n' +
+  '    var difference = new Date() - birth.date;\n' +
+  '    return Math.floor(difference / 31536000000);\n' +
+  '  }\n' +
+  '}';
+
 api.test.case({
   'api.jstp.stringify': [
-    [ jsrsObject, jsrsString ],
+    [ record, recordString ],
   ],
   'api.jstp.parse': [
-    [ jsrsString, jsrsObject ],
-    [ jsrsWhitespacedString, jsrsObject ],
+    [ recordString, record ],
+    [ recordWhitespacedString, record ],
+  ],
+  'api.jstp.interprete': [
+    [ objectStringWithExpressions, object ]
   ],
 });
