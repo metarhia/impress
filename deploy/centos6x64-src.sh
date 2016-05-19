1d30aaecab637e4cebe8c62f6555e3ac090f2c64
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2016-05-19 08:37:19 +0300

    Added 6.2 to deploy scripts, 4.3 and 4.4 to tests

--- deploy/centos6x64-src.sh
+++ deploy/centos6x64-src.sh
@@ -7,15 +7,15 @@ rpm --import http://ftp.mirrorservice.org/sites/ftp.scientificlinux.org/linux/sc
 yum -y install devtoolset-3
 source scl_source enable devtoolset-3
 cd /usr/src
-wget http://nodejs.org/dist/v6.1.0/node-v6.1.0.tar.gz
-tar zxf node-v6.1.0.tar.gz
-rm -f ./node-v6.1.0.tar.gz
-cd node-v6.1.0
+wget http://nodejs.org/dist/v6.2.0/node-v6.2.0.tar.gz
+tar zxf node-v6.2.0.tar.gz
+rm -f ./node-v6.2.0.tar.gz
+cd node-v6.2.0
 ./configure
 make
 make install
 cd ~
-rm -rf /usr/src/node-v6.1.0
+rm -rf /usr/src/node-v6.2.0
 ln -s /usr/local/bin/node /bin
 ln -s /usr/local/bin/npm /bin
 cat >/etc/yum.repos.d/mongodb.repo <<EOL
--- deploy/centos7x64-src.sh
+++ deploy/centos7x64-src.sh
@@ -4,15 +4,15 @@ yum -y install wget mc
 yum -y install psmisc
 yum -y groupinstall "Development Tools"
 cd /usr/src
-wget http://nodejs.org/dist/v6.1.0/node-v6.1.0.tar.gz
-tar zxf node-v6.1.0.tar.gz
-rm -f ./node-v6.1.0.tar.gz
-cd node-v6.1.0
+wget http://nodejs.org/dist/v6.2.0/node-v6.2.0.tar.gz
+tar zxf node-v6.2.0.tar.gz
+rm -f ./node-v6.2.0.tar.gz
+cd node-v6.2.0
 ./configure
 make
 make install
 cd ~
-rm -rf /usr/src/node-v6.1.0
+rm -rf /usr/src/node-v6.2.0
 ln -s /usr/local/bin/node /bin
 ln -s /usr/local/bin/npm /bin
 cat >/etc/yum.repos.d/mongodb.repo <<EOL
