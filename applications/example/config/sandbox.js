e5ad83238919525ac63aa6831dab4b2e763e325f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2016-06-29 00:35:41 +0300

    Global namespaces are rearranged, issue #530
    * Timers moved to api.timers
    * Require deprecated
    * Other issues will be done ASAP

--- applications/example/config/sandbox.js
+++ applications/example/config/sandbox.js
@@ -15,7 +15,7 @@
   api: [
     // Node internal modules
     'console', 'os', 'fs', 'sd', 'tls','net', 'dns', 'url', 'util', 'path', 'zlib', 'http', 'https', 'dgram',
-    'stream', 'buffer', 'domain', 'crypto', 'events', 'punycode', 'readline', 'querystring', 'process',
+    'stream', 'buffer', 'domain', 'crypto', 'events', 'punycode', 'readline', 'querystring', 'process', 'timers',
 
     // Impress API modules
     'db', 'con', 'common', 'impress', 'registry', 'definition',
