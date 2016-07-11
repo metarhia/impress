9dfcb6f1a44d9a2c964f521e2f53b43dfa64b907
Alexey Orlenko <eaglexrlnk@gmail.com>
2016-07-11 16:44:11 +0300

    Implement handling event JSTP packets

---
+++ applications/example/api/interfaceName/sendEvent.js
@@ -0,0 +1,6 @@
+(connection, data, callback) => {
+  connection.event('interfaceName', 'eventName', {
+    example: 'hello'
+  });
+  callback({ success: true });
+}
--- tests/jstp.js
+++ tests/jstp.js
@@ -13,21 +13,27 @@ api.events = require('events');
 require(dir + '/lib/api.common.js');
 require(dir + '/lib/api.jstp.js');
 
-var сonnection = api.jstp.connect('impress', '127.0.0.1', 3000);
+var connection = api.jstp.connect('impress', '127.0.0.1', 3000);
+
+connection.on('event', function(iface, event, args) {
+  console.log('Event', event, 'on interface', iface);
+  console.log('Data:', args);
+});
 
 setTimeout(function() {
 
   console.log('connecting');
-  сonnection.handshake('example', 'user', 'passwordHash', function() {
+  connection.handshake('example', 'user', 'passwordHash', function() {
     console.log('handshake done');
-    сonnection.call('interfaceName', 'methodName', [1, 2, 3], function(res) {
+    connection.call('interfaceName', 'methodName', [1, 2, 3], function(res) {
       console.log('result1 received');
       console.dir(res);
     });
-    сonnection.call('interfaceName', 'methodName', [4, 5, 6], function(res) {
+    connection.call('interfaceName', 'sendEvent', '', function(res) { });
+    connection.call('interfaceName', 'methodName', [4, 5, 6], function(res) {
       console.log('result2 received');
       console.dir(res);
-      сonnection.call('interfaceName', 'methodName', [7, 8, 9], function(res) {
+      connection.call('interfaceName', 'methodName', [7, 8, 9], function(res) {
         console.log('result3 received');
         console.dir(res);
         process.exit(0);
