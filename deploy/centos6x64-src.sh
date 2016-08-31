0fdcb30901c13606a82dfa1872294cc0f65eb21f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2016-09-01 00:23:23 +0300

    Deploy scripts updated to Node 6.5.0

--- deploy/centos6x64-src.sh
+++ deploy/centos6x64-src.sh
@@ -7,15 +7,15 @@ rpm --import http://ftp.mirrorservice.org/sites/ftp.scientificlinux.org/linux/sc
 yum -y install devtoolset-3
 source scl_source enable devtoolset-3
 cd /usr/src
-wget http://nodejs.org/dist/v6.4.0/node-v6.4.0.tar.gz
-tar zxf node-v6.4.0.tar.gz
-rm -f ./node-v6.4.0.tar.gz
-cd node-v6.4.0
+wget http://nodejs.org/dist/v6.5.0/node-v6.5.0.tar.gz
+tar zxf node-v6.5.0.tar.gz
+rm -f ./node-v6.5.0.tar.gz
+cd node-v6.5.0
 ./configure
 make
 make install
 cd ~
-rm -rf /usr/src/node-v6.4.0
+rm -rf /usr/src/node-v6.5.0
 ln -s /usr/local/bin/node /bin
 ln -s /usr/local/bin/npm /bin
 sudo mkdir /ias
