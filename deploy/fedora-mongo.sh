bada3551271b3372fdf03d636ec4039a6991e104
tshemsedinov <timur.shemsedinov@gmail.com>
2016-10-06 03:02:17 +0300

    Fixed DB connection, close #587

--- deploy/fedora-mongo.sh
+++ deploy/fedora-mongo.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-cat >/etc/yum.repos.d/mongodb.repo <<EOL
-[mongodb]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/7Server/mongodb-org/stable/x86_64/
-gpgcheck=0
-enabled=1
-EOL
-sudo dnf install -y mongodb-org
+sudo dnf install -y mongodb-server
 service mongod start
 chkconfig mongod on
