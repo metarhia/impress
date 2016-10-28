8e48f125435c94ad15871977e8a635a05e2cd7b7
Dzyuba Vlad <Dzyubavlad@gmail.com>
2016-10-28 12:53:38 +0300

    Added category, get and new function for mongodb driver

--- applications/example/www/examples/mongodb/getCollections.json/get.js
+++ applications/example/www/examples/mongodb/getCollections.json/get.js
@@ -1,9 +1,21 @@
 (client, callback) => {
-  dbAlias.connection.collections(function(err, collections) {
-    var items = [];
-    for (var i = 0; i < collections.length; i++) {
-      items.push(collections[i].collectionName);
-    }
-    callback(items);
+  dbAlias.Category('smthng', function(err, data) {
+    console.log(err, data);
+    if (err) return callback({ error: err });
+    data.new({ name: 'Vladka', nagibator: 228 }, (err, objectId) => {
+      if (err) return callback({ error: err });
+      callback(objectId);
+    });
+//    var obj = data.get("580f524de5cf05d96d0db30e", (err, data) => {
+//      if (err) return callback({ error: err });
+//      callback(data); 
+//    });
   });
+//  dbAlias.connection.collections(function(err, collections) {
+//    var items = [];
+//    for (var i = 0; i < collections.length; i++) {
+//      items.push(collections[i].collectionName);
+//    }
+//    callback(items);
+//  });
 }
