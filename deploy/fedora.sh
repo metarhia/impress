810b4660dfbf3fa6b57e50bc964d79ead8a6f80a
tshemsedinov <timur.shemsedinov@gmail.com>
2016-10-29 03:21:25 +0300

    Changed Node.js version in deploy scripts to 7.0.0

--- deploy/fedora.sh
+++ deploy/fedora.sh
@@ -3,7 +3,7 @@ dnf -y update
 dnf -y install wget mc
 dnf -y install psmisc
 dnf -y groupinstall "Development Tools"
-curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+curl --silent --location https://rpm.nodesource.com/setup_7.x | bash -
 dnf -y install nodejs
 sudo mkdir /ias
 cd /ias
