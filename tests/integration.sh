b7d187131b3d1cddeea4a0da6a800ab308c80ad8
tshemsedinov <timur.shemsedinov@gmail.com>
2016-11-27 05:33:12 +0200

    Fixed killing workers

--- tests/integration.sh
+++ tests/integration.sh
@@ -1,2 +1,2 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
-node --stack-trace-limit=1000 --allow-natives-syntax $DIR/integration.js
+IMPRESS_MODE=test node --stack-trace-limit=1000 --allow-natives-syntax $DIR/integration.js
