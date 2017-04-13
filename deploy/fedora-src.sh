2336f0d1f00f05ceaf4b3d9233ed599fd7bb4dcd
tshemsedinov <timur.shemsedinov@gmail.com>
2017-04-13 03:38:51 +0300

    Update deploy scripts and TravisCI to use Node.js 7.9.0
    PR-URL: https://github.com/metarhia/Impress/pull/721

--- deploy/fedora-src.sh
+++ deploy/fedora-src.sh
@@ -4,15 +4,15 @@ dnf -y install wget mc gcc-c++
 dnf -y install psmisc
 dnf -y groupinstall "Development Tools"
 cd /usr/src
-wget https://nodejs.org/dist/v7.8.0/node-v7.8.0.tar.gz
-tar zxf node-v7.8.0.tar.gz
-rm -f ./node-v7.8.0.tar.gz
-cd node-v7.8.0
+wget https://nodejs.org/dist/v7.9.0/node-v7.9.0.tar.gz
+tar zxf node-v7.9.0.tar.gz
+rm -f ./node-v7.9.0.tar.gz
+cd node-v7.9.0
 ./configure
 make
 make install
 cd ~
-rm -rf /usr/src/node-v7.8.0
+rm -rf /usr/src/node-v7.9.0
 ln -s /usr/local/bin/node /bin
 ln -s /usr/local/bin/npm /bin
 sudo mkdir /ias
--- deploy/ubuntu-src.sh
+++ deploy/ubuntu-src.sh
@@ -11,15 +11,15 @@ sudo apt-get -y install g++-4.8
 sudo update-alternatives --quiet --install  /usr/bin/g++ g++ /usr/bin/g++-4.8 1
 fi
 cd /usr/src
-sudo wget https://nodejs.org/dist/v7.8.0/node-v7.8.0.tar.gz
-sudo tar zxf node-v7.8.0.tar.gz
-sudo rm -f ./node-v7.8.0.tar.gz
-cd node-v7.8.0
+sudo wget https://nodejs.org/dist/v7.9.0/node-v7.9.0.tar.gz
+sudo tar zxf node-v7.9.0.tar.gz
+sudo rm -f ./node-v7.9.0.tar.gz
+cd node-v7.9.0
 sudo ./configure
 sudo make
 sudo make install
 cd ~
-sudo rm -rf /usr/src/node-v7.8.0
+sudo rm -rf /usr/src/node-v7.9.0
 sudo mkdir /ias
 cd /ias
 sudo npm install nodemailer websocket geoip-lite
