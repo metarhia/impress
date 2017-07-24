cd43a35dfe9b7a692304b94999b3428afb05bcbc
tshemsedinov <timur.shemsedinov@gmail.com>
2017-07-24 14:05:30 +0300

    Update deploy scripts to use Node.js 8.2.1

--- deploy/fedora-src.sh
+++ deploy/fedora-src.sh
@@ -4,15 +4,15 @@ dnf -y install wget mc gcc-c++
 dnf -y install psmisc
 dnf -y groupinstall "Development Tools"
 cd /usr/src
-wget https://nodejs.org/dist/v8.1.2/node-v8.1.2.tar.gz
-tar zxf node-v8.1.2.tar.gz
-rm -f ./node-v8.1.2.tar.gz
-cd node-v8.1.2
+wget https://nodejs.org/dist/v8.2.1/node-v8.2.1.tar.gz
+tar zxf node-v8.2.1.tar.gz
+rm -f ./node-v8.2.1.tar.gz
+cd node-v8.2.1
 ./configure
 make
 make install
 cd ~
-rm -rf /usr/src/node-v8.1.2
+rm -rf /usr/src/node-v8.2.1
 ln -s /usr/local/bin/node /bin
 ln -s /usr/local/bin/npm /bin
 sudo mkdir /ias
