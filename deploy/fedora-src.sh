859f6c7aaba41e028f409d2526f2f6193cbdcaa2
tshemsedinov <timur.shemsedinov@gmail.com>
2017-09-17 00:25:31 +0300

    Version 0.4.27
    - Update dependencies
    - Update deploy scripts
    - Update TravisCI config

--- deploy/fedora-src.sh
+++ deploy/fedora-src.sh
@@ -4,15 +4,15 @@ dnf -y install wget mc gcc-c++
 dnf -y install psmisc
 dnf -y groupinstall "Development Tools"
 cd /usr/src
-wget https://nodejs.org/dist/v8.4.0/node-v8.4.0.tar.gz
-tar zxf node-v8.4.0.tar.gz
-rm -f ./node-v8.4.0.tar.gz
-cd node-v8.4.0
+wget https://nodejs.org/dist/v8.5.0/node-v8.5.0.tar.gz
+tar zxf node-v8.5.0.tar.gz
+rm -f ./node-v8.5.0.tar.gz
+cd node-v8.5.0
 ./configure
 make
 make install
 cd ~
-rm -rf /usr/src/node-v8.4.0
+rm -rf /usr/src/node-v8.5.0
 ln -s /usr/local/bin/node /bin
 ln -s /usr/local/bin/npm /bin
 sudo mkdir /ias
