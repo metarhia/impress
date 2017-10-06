e7581e7949053841335bfd934b4d8fd5a7e9251e
tshemsedinov <timur.shemsedinov@gmail.com>
2017-10-06 18:07:07 +0300

    Update dependencies and deploy scripts
    - Add Nodejs 9.0.0 to TravisCI
    - Update deploy scripts
    
    PR-URL: https://github.com/metarhia/metasync/pull/254

--- deploy/debian-src.sh
+++ deploy/debian-src.sh
@@ -15,15 +15,15 @@ else
 sudo apt-get -y install g++
 fi
 cd /usr/src
-wget https://nodejs.org/dist/v8.5.0/node-v8.5.0.tar.gz
-tar zxf node-v8.5.0.tar.gz
-rm -f ./node-v8.5.0.tar.gz
-cd node-v8.5.0
+wget https://nodejs.org/dist/v9.0.0/node-v9.0.0.tar.gz
+tar zxf node-v9.0.0.tar.gz
+rm -f ./node-v9.0.0.tar.gz
+cd node-v9.0.0
 ./configure
 make
 make install
 cd ~
-rm -rf /usr/src/node-v8.5.0
+rm -rf /usr/src/node-v9.0.0
 sudo mkdir /ias
 cd /ias
 sudo npm install nodemailer websocket geoip-lite
