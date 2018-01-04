d7accc8c992b5264e17e31eaf81eb30f13bb1831
o-rumiantsev <alex.rum12.12.12@gmail.com>
2018-01-04 23:32:13 +0200

    Changed Node.js version in deploy scripts to 9.x

--- deploy/ubuntu.sh
+++ deploy/ubuntu.sh
@@ -3,7 +3,7 @@ RELEASE=$(lsb_release -sc)
 sudo apt-get -y update
 sudo apt-get -y install wget mc curl
 sudo apt-get -y install build-essential openssl libssl-dev pkg-config python
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo mkdir /ias
 cd /ias
