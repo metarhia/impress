a20fb315f648a39e16cc24174d3d5ff014547664
tshemsedinov <timur.shemsedinov@gmail.com>
2018-01-26 19:24:26 +0200

    Version 0.4.31
    PR-URL: https://github.com/metarhia/impress/pull/799

--- deploy/centos7x64-src.sh
+++ deploy/centos7x64-src.sh
@@ -4,15 +4,15 @@ yum -y install wget mc
 yum -y install psmisc
 yum -y groupinstall "Development Tools"
 cd /usr/src
-wget https://nodejs.org/dist/v9.0.0/node-v9.0.0.tar.gz
-tar zxf node-v9.0.0.tar.gz
-rm -f ./node-v9.0.0.tar.gz
-cd node-v9.0.0
+wget https://nodejs.org/dist/v9.4.0/node-v9.4.0.tar.gz
+tar zxf node-v9.4.0.tar.gz
+rm -f ./node-v9.4.0.tar.gz
+cd node-v9.4.0
 ./configure
 make
 make install
 cd ~
-rm -rf /usr/src/node-v9.0.0
+rm -rf /usr/src/node-v9.4.0
 ln -s /usr/local/bin/node /bin
 ln -s /usr/local/bin/npm /bin
 sudo mkdir /ias
--- deploy/fedora-src.sh
+++ deploy/fedora-src.sh
@@ -4,15 +4,15 @@ dnf -y install wget mc gcc-c++
 dnf -y install psmisc
 dnf -y groupinstall "Development Tools"
 cd /usr/src
-wget https://nodejs.org/dist/v9.0.0/node-v9.0.0.tar.gz
-tar zxf node-v9.0.0.tar.gz
-rm -f ./node-v9.0.0.tar.gz
-cd node-v9.0.0
+wget https://nodejs.org/dist/v9.4.0/node-v9.4.0.tar.gz
+tar zxf node-v9.4.0.tar.gz
+rm -f ./node-v9.4.0.tar.gz
+cd node-v9.4.0
 ./configure
 make
 make install
 cd ~
-rm -rf /usr/src/node-v9.0.0
+rm -rf /usr/src/node-v9.4.0
 ln -s /usr/local/bin/node /bin
 ln -s /usr/local/bin/npm /bin
 sudo mkdir /ias
