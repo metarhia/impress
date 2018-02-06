e980525c5e1f5b371fd3ca430c0d2178d5c4e3b8
Mykola Bilochub <nbelochub@gmail.com>
2018-02-06 18:50:57 +0200

    Use sandboxed-fs to limit filesystem access
    Fixes: https://github.com/metarhia/impress/issues/673
    PR-URL: https://github.com/metarhia/impress/pull/802

--- applications/example/www/examples/simple/fsAccess.json/get.js
+++ applications/example/www/examples/simple/fsAccess.json/get.js
@@ -1,5 +1,5 @@
 (client, callback) => {
-  const filePath = application.dir + '/www' + client.path + '/test.txt';
+  const filePath = '/www' + client.path + '/test.txt';
   api.fs.readFile(filePath, 'utf8', (error, data) => {
     callback({ fileContent: data, dataLength: data.length });
   });
