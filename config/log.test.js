dfcb512490e18ee7fdc1b72077a4c55447d62d3d
tshemsedinov <timur.shemsedinov@gmail.com>
2018-04-05 02:43:34 +0300

    Use only logger to output

--- config/log.test.js
+++ config/log.test.js
@@ -4,5 +4,5 @@
   writeInterval: '3s', // Flush log to disk interval
   writeBuffer: 64 * 1024, // Buffer size 64kb
   toFile: ['system', 'fatal', 'error', 'warn', 'info', 'debug', 'slow'],
-  stdout: ['system', 'fatal', 'error']
+  stdout: ['system', 'fatal', 'error', 'warn', 'info', 'debug', 'slow']
 }
--- tests/integration.js
+++ tests/integration.js
@@ -84,14 +84,12 @@ function httpTask(task) {
           request.method + ' ' + request.path +
           ' -> HTTP ' + res.statusCode + ' read: ' + res.socket.bytesRead
         );
-        console.log('  ' + msg);
+        impress.log.debug(msg);
         res.on('error', (err) => {
           if (err) throw err;
         });
       } else {
-        console.log(
-          api.concolor('b,red')('Error: ' + request.method + ' ' + request.path)
-        );
+        impress.log.error('Error: ' + request.method + ' ' + request.path);
         throw new Error('HTTP ' + res.statusCode);
       }
       taskExit();
@@ -106,9 +104,7 @@ function httpTask(task) {
 }
 
 if (process.isMaster) {
-  console.log(
-    api.concolor('b,green')('Testing Impress...')
-  );
+  //impress.log.debug('Testing Impress...');
   impress.server.on('started', () => {
     let i;
     for (i = 0; i < config.tasks.length; i++) {
