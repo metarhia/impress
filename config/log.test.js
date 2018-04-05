2a48bfc28784dea794b0b9b945234578506ee687
tshemsedinov <timur.shemsedinov@gmail.com>
2018-04-05 04:46:37 +0300

    Add access log and application.name
    PR-URL: https://github.com/metarhia/impress/pull/815

--- config/log.test.js
+++ config/log.test.js
@@ -3,6 +3,12 @@
   keepDays: 100, // Delete files after N days
   writeInterval: '3s', // Flush log to disk interval
   writeBuffer: 64 * 1024, // Buffer size 64kb
-  toFile: ['system', 'fatal', 'error', 'warn', 'info', 'debug', 'slow'],
-  stdout: ['system', 'fatal', 'error', 'warn', 'debug']
+  toFile: [
+    'system', 'fatal', 'error', 'warn',
+    'info', 'debug', 'access', 'slow'
+  ],
+  stdout: [
+    'system', 'fatal', 'error', 'warn',
+    'debug'
+  ]
 }
