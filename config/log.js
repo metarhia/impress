83ec5a305d5ac84b0b5934064d8bb30a3547fb95
tshemsedinov <timur.shemsedinov@gmail.com>
2018-04-05 05:03:46 +0300

    fixup! Add access log and application.name

--- config/log.js
+++ config/log.js
@@ -3,6 +3,5 @@
   keepDays: 100, // Delete files after N days
   writeInterval: '3s', // Flush log to disk interval
   writeBuffer: 64 * 1024, // Buffer size 64kb
-  toFile: ['system', 'fatal', 'error', 'warn', 'info', 'debug', 'slow'],
   stdout: ['system', 'fatal', 'error']
 }
