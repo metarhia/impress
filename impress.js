03a3fc7a34abe4619f6c4f961bb56901e87fb757
tshemsedinov <timur.shemsedinov@gmail.com>
2018-04-07 05:33:11 +0300

    Optimize core naming and namespaces

--- impress.js
+++ impress.js
@@ -5,5 +5,5 @@ const fs = require('fs');
 fs.access('./tests', (err) => {
   if (!err) require('./lib/core');
   else require('core'); // eslint-disable-line import/no-unresolved
-  impress.server.start();
+  impress.start();
 });
