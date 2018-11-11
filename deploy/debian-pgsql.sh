364a989b313c80ce0b910d0a4d28731415881747
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2018-11-11 11:35:14 +0200

    fixup! Add deploy scripts for PostgreSQL Delete deploy scripts for MongoDB

--- deploy/debian-pgsql.sh
+++ deploy/debian-pgsql.sh
@@ -3,5 +3,6 @@ RELEASE=$(lsb_release -sc)
 sudo sh -c "echo \"deb http://apt.postgresql.org/pub/repos/apt/ $RELEASE-pgdg main\" >> /etc/apt/sources.list.d/pgdg.list"
 sudo apt update
 sudo apt install -y --force-yes postgresql postgresql-contrib
+# No need to enable and start postgresql.service, because it is enabled and started by default
 cd /ias
 sudo npm install pg
--- deploy/ubuntu-pgsql.sh
+++ deploy/ubuntu-pgsql.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt update;
 sudo apt install -y postgresql postgresql-contrib
+# No need to enable and start postgresql.service, because it is enabled and started by default
 cd /ias
 sudo npm install pg
