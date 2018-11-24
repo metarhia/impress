3035ac309debf52a80b3d464f4093c6d784d82a4
tshemsedinov <timur.shemsedinov@gmail.com>
2018-11-24 02:48:09 +0200

    Remove api.timers namespace
    PR-URL: https://github.com/metarhia/impress/pull/923

--- applications/example/www/examples/tools/longWorker.json/worker.js
+++ applications/example/www/examples/tools/longWorker.json/worker.js
@@ -3,12 +3,12 @@
   let counter = 0;
   console.log('Message from forked worker, will terminate in 30 seconds');
 
-  api.timers.setInterval(() => {
+  setInterval(() => {
     counter++;
     console.log('Next 3 seconds counter: ' + counter);
   }, 3000);
 
-  api.timers.setTimeout(() => {
+  setTimeout(() => {
     console.log('Forked worker terminated after 30 seconds');
     callback();
   }, 30000);
