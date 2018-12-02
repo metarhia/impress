0a43899f8c0b51ec472242ccc8e1b7051ee62897
tshemsedinov <timur.shemsedinov@gmail.com>
2018-12-02 21:52:01 +0200

    fixup! Optimize path concatenation

--- applications/example/www/examples/simple/fsAccess.json/get.js
+++ applications/example/www/examples/simple/fsAccess.json/get.js
@@ -1,5 +1,5 @@
 (client, callback) => {
-  const filePath = '/www' + client.path + '/test.txt';
+  const filePath = api.path.join('/www', client.path, 'test.txt');
   api.fs.readFile(filePath, 'utf8', (error, data) => {
     callback(null, { fileContent: data, dataLength: data.length });
   });
