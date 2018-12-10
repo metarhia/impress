624ac532a0521607099ba62666dec71efd2cf75c
tshemsedinov <timur.shemsedinov@gmail.com>
2018-12-10 22:32:37 +0200

    Optimize sandbox names for databases
    PR-URL: https://github.com/metarhia/impress/pull/981

--- applications/example/www/examples/globalstorage/select.json/get.js
+++ applications/example/www/examples/globalstorage/select.json/get.js
@@ -1,9 +1,7 @@
 (client, callback) => {
-  gs.connection.select('test', {
-    Name: 'test',
-  }).fetch((err, result) => {
+  gs.select('test', { Name: 'test' }).fetch((err, result) => {
     if (err) {
-      callback(err, { error: err.message, keys: Object.keys(gs) });
+      callback(err, { error: err.message });
       return;
     }
     callback(null, { result });
