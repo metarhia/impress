7bcb69b22d4b98b26527be8657c11126f5720f6c
tshemsedinov <timur.shemsedinov@gmail.com>
2018-12-16 22:06:32 +0200

    File upload is broken after refactoring
    Closes: https://github.com/metarhia/impress/issues/995
    PR-URL: https://github.com/metarhia/impress/pull/1007

--- applications/example/www/examples/simple/uploadFile.json/post.js
+++ applications/example/www/examples/simple/uploadFile.json/post.js
@@ -1,11 +1,10 @@
 (client, callback) => {
 
-  callback(err, client.files);
-
   client.upload((err, file) => {
     console.debug({ uploadedFile: file });
   }, (err, count) => {
     console.debug('All ' + count + ' file(s) are uploaded.');
+    callback(err, client.files);
   });
 
 }
