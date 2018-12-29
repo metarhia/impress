34a6abfbd0dd6cae37ea4ca738d2da17ea87ccc1
tshemsedinov <timur.shemsedinov@gmail.com>
2018-12-29 13:07:03 +0200

    Remove handlers: request, end, lazy, error
    Closes: https://github.com/metarhia/impress/issues/1031

--- applications/example/www/examples/cache/htmlPage.ajax/get.js
+++ applications/example/www/examples/cache/htmlPage.ajax/get.js
@@ -1,4 +1,5 @@
 (client, callback) => {
+  client.cache(Duration('30s'));
   console.debug('Page stored in cache for 30 sec');
   callback();
 }
--- applications/example/www/examples/simple/endHandler.json/end.js
+++
@@ -1,6 +0,0 @@
-(client, callback) => {
-  console.debug(
-    'Request finalization handler: end.js, execures after request.js, [verb].js'
-  );
-  callback(null, { handler: 'end' });
-}
