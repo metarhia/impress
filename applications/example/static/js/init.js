279561cbe986cc3f9ad5defd5115e03f1f53f662
tshemsedinov <timur.shemsedinov@gmail.com>
2019-01-02 04:44:14 +0200

    Optimize application example

--- applications/example/static/js/init.js
+++ applications/example/static/js/init.js
@@ -1,52 +1,51 @@
 'use strict';
 
-api.dom.on('load', function() {
+api.dom.on('load', () => {
 
   api.dom.addClass(document.body, 'js');
-  api.dom.fixCookie('SID');
 
-  var panelCenter = api.dom.id('panel-center');
+  const panelCenter = api.dom.id('panel-center');
 
-  var auth = api.ajax({
+  const auth = api.ajax({
     regValidation: { post: '/api/auth/regvalidation.json' },
     signUp: { post: '/api/auth/signUp.json' },
-    signOut: { post: '/api/auth/signOut.json' }
+    signOut: { post: '/api/auth/signOut.json' },
   });
 
   // Auth Module
 
-  api.dom.on('click', '#hmenu-Signin', function() {
-    var closePopup = api.dom.popup('#formLogin');
+  api.dom.on('click', '#hmenu-Signin', () => {
+    const closePopup = api.dom.popup('#formLogin');
     closePopup.closeElement = api.dom.element('#formLoginCancel');
     return false;
   });
 
-  api.dom.on('click', '#hmenu-Signout', function() {
-    auth.signOut({}, function(/*err, data*/) {
+  api.dom.on('click', '#hmenu-Signout', () => {
+    auth.signOut({}, (/*err, data*/) => {
       if (localStorage) localStorage.clear();
       window.location.reload(true);
     });
     return false;
   });
 
-  api.dom.on('click', '#hmenu-SignUp', function() {
-    var closePopup = api.dom.popup('#formReg');
+  api.dom.on('click', '#hmenu-SignUp', () => {
+    const closePopup = api.dom.popup('#formReg');
     closePopup.closeElement = api.dom.element('#formRegCancel');
     return false;
   });
 
-  api.dom.on('click', '#formRegDo', function(/*event*/) {
-    var inputEmail = api.dom.id('formRegEmail'),
-        inputPassword = api.dom.id('formRegPassword'),
-        data = { email: inputEmail.value };
-    auth.regValidation(data, function(err, json) {
+  api.dom.on('click', '#formRegDo', (/*event*/) => {
+    const inputEmail = api.dom.id('formRegEmail');
+    const inputPassword = api.dom.id('formRegPassword');
+    const data = { email: inputEmail.value };
+    auth.regValidation(data, (err, json) => {
       console.dir(json);
       if (json) {
         data.password = inputPassword.value;
         console.dir(data);
         if (json.email) {
           api.dom.removeClass(inputEmail, 'invalid');
-          auth.signUp(data, function(err, data) {
+          auth.signUp(data, (err, data) => {
             if (data.result === 'ok') window.location.reload(true);
           });
         } else {
@@ -58,95 +57,116 @@ api.dom.on('load', function() {
     return false;
   });
 
-  api.dom.on('click', '#formLoginSignIn', function() {
-    var btn = api.dom.id('formLoginSubmit');
+  api.dom.on('click', '#formLoginSignIn', () => {
+    const btn = api.dom.id('formLoginSubmit');
     api.dom.fireEvent(btn, 'click');
   });
 
   // Left menu
 
-  api.dom.on('click', '#menuAJAX', function() {
-    var parameterName = 'paramaterValue';
-    api.dom.load('/examples/simple/ajaxTest.ajax?parameterName=' + parameterName, panelCenter);
+  api.dom.on('click', '#menuAJAX', () => {
+    const parameterName = 'paramaterValue';
+    api.dom.load(
+      '/examples/simple/ajaxTest.ajax?parameterName=' + parameterName,
+      panelCenter
+    );
   });
 
-  api.dom.on('click', '#menuGetJSON', function() {
-    var parameterName = 'paramaterValue';
-    panelCenter.innerHTML ='<div class="progress"></div>';
-    api.ajax.get('/examples/simple/jsonGet.json', { parameterName: parameterName }, function(err, res) {
-      panelCenter.innerHTML = '<pre>' + JSON.stringify(res, null, 2) + '</pre>';
-    });
-  });
-
-  api.dom.on('click', '#menuPostJSON', function() {
-    var parameterName = 'paramaterValue';
+  api.dom.on('click', '#menuGetJSON', () => {
+    const parameterName = 'paramaterValue';
     panelCenter.innerHTML = '<div class="progress"></div>';
-    api.ajax.post('/examples/simple/jsonPost.json', { parameterName: parameterName }, function(err, res) {
-      panelCenter.innerHTML = '<pre>' + JSON.stringify(res, null, 2) + '</pre>';
-    });
+    api.ajax.get(
+      '/examples/simple/jsonGet.json',
+      { parameterName },
+      (err, res) => {
+        panelCenter.innerHTML = '<pre>' +
+          JSON.stringify(res, null, 2) + '</pre>';
+      }
+    );
   });
 
-  api.dom.on('click', '#menuForkWorker', function() {
-    api.ajax.get('/examples/tools/forkWorker.json', function() {
+  api.dom.on('click', '#menuPostJSON', () => {
+    const parameterName = 'paramaterValue';
+    panelCenter.innerHTML = '<div class="progress"></div>';
+    api.ajax.post(
+      '/examples/simple/jsonPost.json',
+      { parameterName },
+      (err, res) => {
+        panelCenter.innerHTML = '<pre>' +
+          JSON.stringify(res, null, 2) + '</pre>';
+      }
+    );
+  });
+
+  api.dom.on('click', '#menuForkWorker', () => {
+    api.ajax.get('/examples/tools/forkWorker.json', () => {
       panelCenter.innerHTML = 'Worker process forked, see console for output.';
     });
   });
 
-  api.dom.on('click', '#menuLongWorker', function() {
-    api.ajax.get('/examples/tools/longWorker.json', function() {
-      panelCenter.innerHTML = 'Worker process forked and will terminate in 30 seconds, see console for output.';
+  api.dom.on('click', '#menuLongWorker', () => {
+    api.ajax.get('/examples/tools/longWorker.json', () => {
+      panelCenter.innerHTML = 'Worker process forked and ' +
+        'will terminate in 30 seconds, see console for output.';
     });
   });
 
-  api.dom.on('click', '#menuTemplate', function() {
+  api.dom.on('click', '#menuTemplate', () => {
     window.location = '/examples/override';
   });
 
-  api.dom.on('click', '#menuFileUpload', function() {
+  api.dom.on('click', '#menuFileUpload', () => {
     api.dom.load('/examples/simple/upload.ajax', panelCenter);
   });
 
-  api.dom.on('click', '#menuDownload', function() {
-    panelCenter.innerHTML = '<iframe src="/examples/simple/download.ajax" style="display:none"></iframe>';
+  api.dom.on('click', '#menuDownload', () => {
+    panelCenter.innerHTML = '<iframe src="/examples/simple/download.ajax" ' +
+      'style="display:none"></iframe>';
   });
 
-  api.dom.on('click', '#menuGeoIP', function() {
+  api.dom.on('click', '#menuGeoIP', () => {
     panelCenter.innerHTML = '<div class="progress"></div>';
-    api.ajax.get('/examples/tools/geoip.json', function(err, res) {
+    api.ajax.get('/examples/tools/geoip.json', (err, res) => {
       panelCenter.innerHTML = '<pre>' + JSON.stringify(res, null, 2) + '</pre>';
     });
   });
 
-  var ws;
-  api.dom.on('click', '#menuWS', function() {
+  let ws;
+  api.dom.on('click', '#menuWS', () => {
     ws = api.ws('/examples/events/connect.ws');
 
     panelCenter.innerHTML = (
-      '<a class="button silver" id="btnWsClose"><span class="icon delete"></span>Close WebSocket connection</a> ' +
-      '<a class="button silver" id="btnWsSend"><span class="icon handshake"></span>Send "Hello" to WebSocket</a>' +
+      '<a class="button silver" id="btnWsClose">' +
+      '<span class="icon delete"></span>Close WebSocket connection</a> ' +
+      '<a class="button silver" id="btnWsSend">' +
+      '<span class="icon handshake"></span>Send "Hello" to WebSocket</a>' +
       '<hr>Connecting...<hr>'
     );
-    var btnWsSend = api.dom.id('btnWsSend');
+    const btnWsSend = api.dom.id('btnWsSend');
 
-    ws.on('open', function() {
+    ws.on('open', () => {
       panelCenter.insertAdjacentHTML('beforeend', 'Connection opened<hr>');
     });
 
-    ws.on('close', function() {
+    ws.on('close', () => {
       panelCenter.insertAdjacentHTML('beforeend', 'Connection closed<hr>');
     });
 
-    ws.on('message', function(event) {
-      panelCenter.insertAdjacentHTML('beforeend', 'Message from server: ' + event.data + '<hr>');
+    ws.on('message', (event) => {
+      panelCenter.insertAdjacentHTML(
+        'beforeend', 'Message from server: ' + event.data + '<hr>'
+      );
     });
 
-    api.dom.on('click', '#btnWsClose', function() {
+    api.dom.on('click', '#btnWsClose', () => {
       ws.close();
       btnWsSend.style.display = 'none';
     });
 
-    api.dom.on('click', '#btnWsSend', function() {
-      panelCenter.insertAdjacentHTML('beforeend', 'Sending to server: Hello<hr>');
+    api.dom.on('click', '#btnWsSend', () => {
+      panelCenter.insertAdjacentHTML(
+        'beforeend', 'Sending to server: Hello<hr>'
+      );
       ws.send('Hello');
     });
   });
@@ -162,10 +182,10 @@ api.dom.on('load', function() {
   }
 
   function jstpConnect() {
-    var messageBlock = api.dom.id('jstpLog');
+    const messageBlock = api.dom.id('jstpLog');
 
-    function print() {
-      var msg = Array.prototype.join.call(arguments, ' ');
+    function print(...args) {
+      const msg = args.join(' ');
       if (messageBlock.innerText) {
         messageBlock.innerText += msg;
       } else {
@@ -177,7 +197,7 @@ api.dom.on('load', function() {
     api.jstp.ws.connect('example',
       new api.jstp.Application('example', {}),
       'ws://localhost:8000',
-      function(error, connection, session) {
+      (error, connection, session) => {
         if (error) {
           print(error);
           return;
@@ -204,11 +224,11 @@ api.dom.on('load', function() {
         return;
       }
 
-      interfaceName.on('eventName', function(args) {
+      interfaceName.on('eventName', (args) => {
         print('Got event, data:', JSON.stringify(args));
       });
 
-      interfaceName.methodName(1, 2, 3, function(err, res) {
+      interfaceName.methodName(1, 2, 3, (err, res) => {
         if (err) {
           print(err);
           return;
@@ -217,21 +237,21 @@ api.dom.on('load', function() {
         print(res);
       });
 
-      interfaceName.sendEvent(function(err) {
+      interfaceName.sendEvent((err) => {
         if (err) {
           print(err);
           return;
         }
       });
 
-      interfaceName.methodName(4, 5, 6, function(err, res) {
+      interfaceName.methodName(4, 5, 6, (err, res) => {
         if (err) {
           print(err);
           return;
         }
         print('result2 received');
         print(res);
-        interfaceName.methodName(7, 8, 9, function(err, res) {
+        interfaceName.methodName(7, 8, 9, (err, res) => {
           if (err) {
             print(err);
             return;
@@ -243,7 +263,7 @@ api.dom.on('load', function() {
     }
   }
 
-  api.dom.on('click', '#menuAuth', function() {
+  api.dom.on('click', '#menuAuth', () => {
     window.location = '/examples/auth';
   });
 
