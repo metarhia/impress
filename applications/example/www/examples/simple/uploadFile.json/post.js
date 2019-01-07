b19dbacf247961e5024f1588d164550baa67d4af
tshemsedinov <timur.shemsedinov@gmail.com>
2019-01-07 17:39:23 +0200

    Cleanup example application

--- applications/example/www/examples/simple/uploadFile.json/post.js
+++ applications/example/www/examples/simple/uploadFile.json/post.js
@@ -3,7 +3,7 @@
   client.upload((err, file) => {
     console.debug({ uploadedFile: file });
   }, (err, count) => {
-    console.debug('All ' + count + ' file(s) are uploaded.');
+    console.debug(`All ${count} file(s) are uploaded.`);
     callback(err, client.files);
   });
 
