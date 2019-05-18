7d42126cb05ad07ac7dd23961364d147eed391de
Denys Otrishko <shishugi@gmail.com>
2019-05-18 14:29:08 +0300

    Replace application.frontend/application.backend -> application.cloud
    This replaces 'backend' and 'frontend' event emitters from Application
    with 'cloud'. This also includes a few fixes for cloud.js.

--- applications/example/test/jstp/frontendEvent.test.js
+++ applications/example/test/jstp/frontendEvent.test.js
@@ -1,8 +1,8 @@
-application.cloud.once('cloudWorkerHello', data => {
+application.frontend.once('frontendWorkerHello', data => {
   test.strictSame(data, { answer: 42 });
   test.end();
 });
 
 api.jstpTest.connect(test.cbFail((conn, app) => {
-  app.interfaceName.cloudEmitEvent(test.cb());
+  app.interfaceName.frontendEmitEvent(test.cb());
 }));
