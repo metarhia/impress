fc66aef478f8096583dcf76945077fcddffe1c1c
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2019-12-31 02:44:30 +0200

    Drop deploy scripts

--- deploy/fedora-pgsql.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo dnf -y install postgresql postgresql-server postgresql-contrib
-sudo postgresql-setup --initdb --unit postgresql
-sudo systemctl enable --now postgresql
-cd /ias
-sudo npm install pg
