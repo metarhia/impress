b8c9709bcdefe1fe2275ff576b8080472a2119e8
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2019-12-31 02:44:30 +0200

    Drop deploy scripts

--- deploy/ubuntu-pgsql.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt update;
-sudo apt install -y postgresql postgresql-contrib
-# No need to enable and start postgresql.service, because it is enabled and started by default
-cd /ias
-sudo npm install pg
