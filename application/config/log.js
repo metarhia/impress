71f87049ed60e438151e883179b102c9b0454258
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-05-21 22:00:53 +0300

    Remove application example
    Application example will be published in https://github.com/metarhia/StarterKit
    PR-URL: https://github.com/metarhia/impress/pull/1199

--- application/config/log.js
+++
@@ -1,6 +0,0 @@
-({
-  keepDays: 100, // Delete files after N days
-  writeInterval: Duration('3s'), // Flush log to disk interval
-  writeBuffer: 64 * 1024, // Buffer size 64kb
-  toStdout: ['system', 'fatal', 'error']
-});
--- application/data/.gs
+++
@@ -1,1 +0,0 @@
-{server:'0',count:0,size:0,next:0}
