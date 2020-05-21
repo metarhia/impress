698e2dd268148aaf48f0011de6685389514c5eca
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-05-22 01:37:02 +0300

    Remove server start scripts

--- server.sh
+++
@@ -1,7 +0,0 @@
-sudo SERVER_MODE=debug node --stack-trace-limit=1000 --max_old_space_size=2048 --no-warnings server.js
-
-# Add following parameter to extend process memory to 2 Gb
-# --max_old_space_size=2048
-
-# Add following parameter to disable automatic GC and call gs() manually
-# --nouse-idle-notification --expose-gc
