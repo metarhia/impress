32354bf80b40585923aa650787fdf9c9c08afa88
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-05-21 21:58:16 +0300

    Code from HowProgrammingWorks/NodejsStarterKit
    See: https://github.com/HowProgrammingWorks/NodejsStarterKit

--- impress.js
+++ impress.js
@@ -1,3 +1,37 @@
 'use strict';
 
-module.exports = require('./lib/core.js');
+const { Worker } = require('worker_threads');
+const path = require('path');
+
+const Config = require('@metarhia/config');
+
+const PATH = process.cwd();
+const CFG_PATH = path.join(PATH, 'config');
+
+(async () => {
+  const config = await new Config(CFG_PATH);
+  const { sections } = config;
+  const count = sections.server.ports.length;
+  const workers = new Array(count);
+
+  const start = id => {
+    const workerPath = path.join(__dirname, 'lib/worker.js');
+    const worker = new Worker(workerPath);
+    workers[id] = worker;
+    worker.on('exit', code => {
+      if (code !== 0) start(id);
+    });
+  };
+
+  for (let id = 0; id < count; id++) start(id);
+
+  const stop = async () => {
+    console.log();
+    for (const worker of workers) {
+      worker.postMessage({ name: 'stop' });
+    }
+  };
+
+  process.on('SIGINT', stop);
+  process.on('SIGTERM', stop);
+})();
--- server.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-const dev = fs.existsSync('impress.js');
-const impress = require(dev ? '.' : 'impress');
-impress.start();
