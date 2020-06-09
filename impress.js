f24c6be0186f4bac4db06f4a49b9142322811f7c
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-06-10 00:46:14 +0300

    Disable terminal and exit on CTRL-C
    Closes: https://github.com/metarhia/impress/issues/1213
    PR-URL: https://github.com/metarhia/impress/pull/1224

--- impress.js
+++ impress.js
@@ -9,11 +9,13 @@ const Config = require('@metarhia/config');
 
 const PATH = process.cwd();
 const CFG_PATH = path.join(PATH, 'application/config');
+const CTRL_C = 3;
 
 (async () => {
   const config = await new Config(CFG_PATH);
   const { sections } = config;
   const count = sections.server.ports.length;
+  let active = count;
   const workers = new Array(count);
 
   const start = id => {
@@ -22,13 +24,13 @@ const CFG_PATH = path.join(PATH, 'application/config');
     workers[id] = worker;
     worker.on('exit', code => {
       if (code !== 0) start(id);
+      else if (--active === 0) process.exit(0);
     });
   };
 
   for (let id = 0; id < count; id++) start(id);
 
   const stop = async () => {
-    console.log();
     for (const worker of workers) {
       worker.postMessage({ name: 'stop' });
     }
@@ -36,4 +38,12 @@ const CFG_PATH = path.join(PATH, 'application/config');
 
   process.on('SIGINT', stop);
   process.on('SIGTERM', stop);
+
+  if (process.stdin.isTTY) {
+    process.stdin.setRawMode(true);
+    process.stdin.on('data', data => {
+      const key = data[0];
+      if (key === CTRL_C) stop();
+    });
+  }
 })();
