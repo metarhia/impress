d7360fb948935abe4b1e349d16d7e9e67ea7ca78
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-08-27 19:22:17 +0300

    Create additional workers without server
    Refs: https://github.com/metarhia/impress/issues/1314
    Closes: https://github.com/metarhia/impress/issues/1311

--- impress.js
+++ impress.js
@@ -14,8 +14,9 @@ const options = { trackUnmanagedFds: true };
 
 (async () => {
   const config = await new Config(CFG_PATH);
+  const { pool } = config.sections.server.workers;
   const balancer = config.sections.server.balancer ? 1 : 0;
-  const count = config.sections.server.ports.length + balancer;
+  const count = config.sections.server.ports.length + balancer + pool;
   let active = count;
   const workers = new Array(count);
 
