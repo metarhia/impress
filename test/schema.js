c29ca6a35676f4fa9a9d6b0dd9cec9a5292878c3
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-01-11 23:52:17 +0200

    Schema class for method metadata

---
+++ test/schema.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const metatests = require('metatests');
+const { Schema } = require('../lib/schema.js');
+
+metatests.test('lib/schema', (test) => {
+  const definition = {
+    field1: 'string',
+    field2: { type: 'number' },
+  };
+  const schema = new Schema('example', definition);
+  test.strictSame(schema.fields.field1.type, 'string');
+  test.strictSame(schema.fields.field2.requered, true);
+  test.end();
+});
