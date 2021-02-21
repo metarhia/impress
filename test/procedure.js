d90bc8ae3fa18dfc6b12c11646ab64fbc09ccf8a
vara855 <cannon22222@gmail.com>
2021-02-21 10:54:46 +0300

    lib/procedure - add validate functionality

---
+++ test/procedure.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const metatests = require('metatests');
+const { Procedure } = require('../lib/procedure');
+
+metatests.testAsync('lib/procedure validate', async (test) => {
+  const script = () => ({
+    validate: ({ a, b }) => {
+      if (a % 3 === 0) throw new Error('Expected `a` to be multiple of 3');
+      if (b % 5 === 0) throw new Error('Expected `b` to be multiple of 5');
+    },
+
+    method: async ({ a, b }) => {
+      const result = a + b;
+      return result;
+    },
+  });
+  const procedure = new Procedure(script, require('../lib/application'));
+
+  test.rejects(
+    () => procedure.invoke({}, { a: 3, b: 6 }),
+    /to be multiple of 3/
+  );
+
+  test.resolves(() => procedure.invoke({}, { a: 4, b: 6 }), 10);
+  test.end();
+});
