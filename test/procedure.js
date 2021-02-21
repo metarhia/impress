c129c0fed42215b1b4a257dec2ad68311cd62795
vara855 <cannon22222@gmail.com>
2021-02-21 10:54:46 +0300

    Schema custom validation method
    Closes: https://github.com/metarhia/impress/issues/1480
    
    PR-URL: https://github.com/metarhia/impress/pull/1489

---
+++ test/procedure.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const metatests = require('metatests');
+const { Procedure } = require('../lib/procedure.js');
+
+metatests.testAsync('lib/procedure validate', async (test) => {
+  const script = () => ({
+    validate: ({ a, b }) => {
+      if (a % 3 === 0) throw new Error('Expected `a` to be multiple of 3');
+      if (b % 5 === 0) throw new Error('Expected `b` to be multiple of 5');
+    },
+
+    method: async ({ a, b }) => {
+      const result = a + b;
+      return result;
+    },
+  });
+
+  const application = {
+    Error,
+    server: {
+      semaphore: {
+        async enter() {},
+        leave() {},
+      },
+    },
+  };
+  const procedure = new Procedure(script, application);
+
+  test.rejects(
+    () => procedure.invoke({}, { a: 3, b: 6 }),
+    /to be multiple of 3/
+  );
+
+  test.resolves(() => procedure.invoke({}, { a: 4, b: 6 }), 10);
+  test.end();
+});
