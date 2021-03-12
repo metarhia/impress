1b559d600d95b46599c5913c78e972a6fe1a8a6d
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-03-12 22:48:59 +0200

    Fix config schema
    Refs: https://github.com/metarhia/impress/issues/874
    Refs: https://github.com/metarhia/impress/issues/1216

--- schemas/config/log.js
+++ schemas/config/log.js
@@ -2,12 +2,6 @@
   keepDays: { type: 'number', default: 100 },
   writeInterval: { type: 'number', default: 3000 },
   writeBuffer: { type: 'number', default: 64 * 1024 },
-  toFile: {
-    type: { array: 'string' },
-    default: ['error', 'warn', 'info', 'debug', 'log'],
-  },
-  toStdout: {
-    type: { array: 'string' },
-    default: ['error', 'warn', 'info', 'debug', 'log'],
-  },
+  toFile: { array: 'string' },
+  toStdout: { array: 'string' },
 });
