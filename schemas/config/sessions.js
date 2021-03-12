1f28af41b6e562a2a79b4d3a507892bec87fd510
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-03-12 16:36:23 +0200

    Improve schemas for configs and contracts
    Refs: https://github.com/metarhia/impress/issues/874

--- schemas/config/sessions.js
+++ schemas/config/sessions.js
@@ -8,7 +8,7 @@
   secret: 'string',
   regenerate: { type: 'number', default: 60 * 60 * 1000 },
   expire: { type: 'number', default: 24 * 60 * 60 * 1000 },
-  persistent: { type: 'boolean', default: true },
+  persistent: 'boolean',
   limits: {
     ip: { type: 'number', default: 20 },
     user: { type: 'number', default: 5 },
