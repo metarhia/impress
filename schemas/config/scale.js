19cc46ae425dfb88b10850ec45a8ad30773afc4c
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-03-13 23:23:20 +0200

    Update schemas
    Refs: https://github.com/metarhia/impress/issues/1519
    Refs: https://github.com/metarhia/impress/issues/1518
    Refs: https://github.com/metarhia/impress/issues/1517
    Refs: https://github.com/metarhia/impress/issues/1399
    Refs: https://github.com/metarhia/impress/issues/1214

--- schemas/config/scale.js
+++ schemas/config/scale.js
@@ -3,5 +3,5 @@
   server: 'string',
   instance: { enum: ['standalone', 'controller', 'server'] },
   token: { type: 'string', length: 32 },
-  gc: { type: 'number', default: 60 * 60 * 1000 },
+  gc: 'number',
 });
