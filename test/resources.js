0110023e9ce07c78598f4361d981078bf60cccc3
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-03-28 01:53:07 +0200

    fixup! Resources class tests

--- test/resources.js
+++ test/resources.js
@@ -5,12 +5,17 @@ const metatests = require('metatests');
 const { Resources } = require('../lib/resources.js');
 
 const root = process.cwd();
-const resourcesPath = path.join(root, 'test/cache');
+
+const application = {
+  path: path.join(root, 'test'),
+  absolute(relative) {
+    return path.join(this.path, relative);
+  },
+};
 
 metatests.testAsync('lib/resources load', async (test) => {
-  const resources = new Resources(resourcesPath);
+  const resources = new Resources('cache', application);
   await resources.load();
-  test.strictSame(resources.path, resourcesPath);
   test.strictSame(resources.get('/example/add.js').length, 158);
   test.end();
 });
