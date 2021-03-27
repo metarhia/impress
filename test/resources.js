c442782cd28cce5c8c14e8a2d245e34f9878bd26
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-03-27 23:38:48 +0200

    Resources class tests
    Refs: https://github.com/metarhia/impress/issues/1534
    
    PR-URL: https://github.com/metarhia/impress/pull/1537

---
+++ test/resources.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const path = require('path');
+const metatests = require('metatests');
+const { Resources } = require('../lib/resources.js');
+
+const root = process.cwd();
+
+const application = {
+  path: path.join(root, 'test'),
+  absolute(relative) {
+    return path.join(this.path, relative);
+  },
+};
+
+metatests.testAsync('lib/resources load', async (test) => {
+  const resources = new Resources('cache', application);
+  await resources.load();
+  test.strictSame(resources.get('/example/add.js').length, 158);
+  test.end();
+});
