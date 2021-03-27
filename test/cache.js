c6c69ff9392dbb22b47db07aa65d3bf6e016f522
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-03-27 23:19:18 +0200

    Extract class Resources from Application
    Refs: https://github.com/metarhia/impress/issues/1534

--- test/cache.js
+++ test/cache.js
@@ -9,7 +9,7 @@ const cachePath = path.join(root, 'test/cache');
 
 metatests.testAsync('lib/cache load', async (test) => {
   const cache = new Cache(cachePath);
-  cache.load();
+  await cache.load();
   test.strictSame(cache.path, cachePath);
   test.strictSame(typeof cache.tree, 'object');
   test.end();
