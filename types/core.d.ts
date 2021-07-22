efd1ccf07b889ba66ec5a603f90bb7915c8d94d6
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-07-22 03:06:24 +0300

    Typings for Scheduler and Task
    Refs: https://github.com/metarhia/impress/issues/1409

--- types/core.d.ts
+++ types/core.d.ts
@@ -1,9 +1,23 @@
+export interface Task {
+  name: string;
+  every: string;
+  args: object;
+  run: string;
+}
+
+export interface Scheduler {
+  add(task: Task): void;
+  remove(id: string): void;
+  stop(name: string): void;
+}
+
 export interface Application {
   worker: object;
   server: object;
   auth: object;
   resources: Map<string, Buffer>;
   introspect: () => Promise<any>;
+  scheduler: Scheduler;
 }
 
 export interface Context {
