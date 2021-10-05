bdfb0a4847bd3f7a3bb5d59782e92d8aabf95d51
Andrey Sakharov <sakharov.a@n-t.io>
2021-10-05 11:23:08 +0300

    Typings: a "process" member was defined for the "node" namespace

--- test/dependencies.js
+++ test/dependencies.js
@@ -5,6 +5,9 @@ const dependencies = require('../lib/dependencies.js');
 
 metatests.test('lib/dependencies', async (test) => {
   test.strictSame(typeof dependencies.node.os, 'object');
+  test.strictSame(typeof dependencies.node.process, 'object');
+  test.strictSame(typeof dependencies.node.process.cwd, 'function');
+  test.strictSame(typeof dependencies.node.process.cwd(), 'string');
   test.strictSame(typeof dependencies.npm.ws, 'function');
   test.strictSame(typeof dependencies.metarhia.metaschema, 'object');
   test.end();
--- types/impress.d.ts
+++ types/impress.d.ts
@@ -91,6 +91,7 @@ declare global {
     const https: typeof _https;
     const http2: typeof _http2;
     const dgram: typeof _dgram;
+    const process: NodeJS.Process;
   }
 
   namespace npm {
