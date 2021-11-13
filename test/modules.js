95f590fa9aeec89028310eaeec944a244a1255ab
georgolden <georgoldenb@gmail.com>
2021-11-13 09:38:44 +0100

    wip: inThread syntax for async func in Modules. Working solution
    	modified:   lib/application.js
    	modified:   lib/modules.js
    	new file:   log
    	modified:   test/modules.js

--- test/modules.js
+++ test/modules.js
@@ -26,27 +26,6 @@ metatests.testAsync('lib/modules load', async (test) => {
   const cache = new Modules('cache', application);
   await cache.load();
   test.strictSame(cache.tree.utils.UNITS.length, 9);
-
-  const { example } = cache.tree;
-  const allFromThread = async () => {
-    const result = await Promise.all([
-      example.thread.method1({ val: '2' }).inThread({ exclusive: true }),
-      example.thread.method1({ val: '2' }).inThread({ exclusive: false }),
-      example.thread.method1({ val: '2' }),
-      example.thread.method2({ val: '2' }).catch((err) => {
-        test.strictSame(err.message, 'I AM ERROR');
-      }),
-    ]);
-    return { result };
-  };
-
-  const { result } = await allFromThread();
-
-  console.dir({ result });
-
-  for (let i = 0; i < result.length - 1; i++) {
-    test.strictSame(result[i], { val: '2' });
-  }
-
+  console.dir({ cache }, { depth: 10 });
   test.end();
 });
