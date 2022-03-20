1f6dd831c7db4bbf8e190fb9dee0a345fcf044be
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-03-20 20:20:33 +0200

    Fix test example: asunc start function

--- test/cache/example/start.js
+++ test/cache/example/start.js
@@ -1,12 +1,8 @@
-({
-  privateField: 100,
-
-  async method() {
-    if (application.worker.id === 'W1') {
-      console.debug('Start example plugin');
-      this.parent.cache.set({ key: 'keyName', val: this.privateField });
-      const res = lib.example.cache.get({ key: 'keyName' });
-      console.debug({ res, cache: this.parent.cache.values });
-    }
-  },
-});
+async () => {
+  if (application.worker.id === 'W1') {
+    console.debug('Start example plugin');
+    this.parent.cache.set({ key: 'keyName', val: 'value' });
+    const res = lib.example.cache.get({ key: 'keyName' });
+    console.debug({ res, cache: this.parent.cache.values });
+  }
+};
