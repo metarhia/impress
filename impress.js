17a20140284eccbf08e71a26490d9326864e9c61
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-05-15 02:17:49 +0300

    Worker-based multitenancy implementation

--- impress.js
+++ impress.js
@@ -2,6 +2,7 @@
 
 process.title = 'impress';
 
+const fsp = require('fs').promises;
 const { Worker } = require('worker_threads');
 const path = require('path');
 const { Config } = require('metaconfiguration');
@@ -9,153 +10,199 @@ const metavm = require('metavm');
 const metautil = require('metautil');
 const { loadSchema } = require('metaschema');
 const { Logger } = require('metalog');
-let logger = null;
-
-let finalization = false;
-let initialization = true;
 
 const CONFIG_SECTIONS = ['log', 'scale', 'server', 'sessions'];
 const PATH = process.cwd();
+const WORKER_PATH = path.join(__dirname, 'lib/worker.js');
 const CFG_PATH = path.join(PATH, 'application/config');
 const LOG_PATH = path.join(PATH, 'log');
 const CTRL_C = 3;
 const LOG_OPTIONS = { path: LOG_PATH, home: PATH, workerId: 0 };
+const CONTEXT = metavm.createContext({ process });
+const CFG_OPTIONS = { mode: process.env.MODE, context: CONTEXT };
 
-const exit = async (message = 'Can not start Application server') => {
-  console.error(metautil.replace(message, PATH, ''));
-  if (logger) await logger.close();
+const impress = {
+  logger: null,
+  config: null,
+  finalized: () => {},
+  finalization: false,
+  initialization: true,
+  console,
+  applications: new Map(),
+  lastWorkerId: 0,
+};
+
+const exit = async (message) => {
+  impress.console.info(message);
+  if (impress.logger && impress.logger.active) await this.logger.close();
   process.exit(1);
 };
 
 const logError = (type) => (err) => {
   const msg = err.stack || err.message || 'no stack trace';
-  console.error(`${type} error: ${msg}`);
-  if (finalization) return;
-  if (initialization) exit();
+  impress.console.error(`${type}: ${msg}`);
+  if (impress.finalization) return;
+  if (impress.initialization) exit('Can not start Application server');
 };
 
-process.on('uncaughtException', logError('uncaughtException'));
-process.on('warning', logError('warning'));
-process.on('unhandledRejection', logError('unhandledRejection'));
+process.on('uncaughtException', logError('Uncaught exception'));
+process.on('warning', logError('Warning'));
+process.on('unhandledRejection', logError('Unhandled rejection'));
+
+const startWorker = (app, kind, port, id = ++impress.lastWorkerId) => {
+  // TODO: implement kind: 'worker'
+  // 'scheduler' will not be kind of worker
+  // it will be shared for all apps
+  const workerData = { id, kind, path: app.path, port };
+  const options = { trackUnmanagedFds: true, workerData };
+  const worker = new Worker(WORKER_PATH, options);
+  app.threads.set(id, worker);
+
+  worker.on('exit', (code) => {
+    if (code !== 0) startWorker(app, kind, port, id);
+    else app.threads.delete(id);
+    if (app.threads.size === 0) {
+      impress.applications.delete(app.path);
+      if (impress.applications.size === 0) impress.finalized();
+    }
+  });
+
+  const handlers = {
+    event: ({ name }) => {
+      if (name === 'started') {
+        app.ready++;
+        app.pool.add(worker);
+        if (app.threads.size === app.ready) {
+          impress.console.info(`App started: ${app.peth}`);
+        }
+      }
+    },
+
+    task: (msg) => {
+      impress.console.log({ id, type: 'task', msg });
+      //const transferList = msg.port ? [msg.port] : undefined;
+      //scheduler.postMessage(msg, transferList);
+    },
+
+    invoke: async (msg) => {
+      impress.console.log({ id, type: 'invoke', msg });
+      /*
+      const { name, port, exclusive } = msg;
+      if (name === 'done') {
+        if (exclusive) pool.release(worker);
+        return;
+      }
+      if (name !== 'request') return;
+      const promisedThread = exclusive ? pool.capture() : pool.next();
+      const next = await promisedThread.catch(() => {
+        port.postMessage({ error: new Error('No thread available') });
+        return null;
+      });
+      if (!next) return;
+      next.postMessage(msg, [port]);
+      */
+    },
+  };
+
+  worker.on('message', (msg) => {
+    const handler = handlers[msg.type];
+    if (handler) handler(msg);
+  });
+};
 
 const validateConfig = async (config) => {
-  const schemaPath = path.join(__dirname, 'schemas/config');
   let valid = true;
+  const schemaPath = path.join(__dirname, 'schemas/config');
   for (const section of CONFIG_SECTIONS) {
     const fileName = path.join(schemaPath, section + '.js');
     const schema = await loadSchema(fileName);
     const checkResult = schema.check(config[section]);
     if (!checkResult.valid) {
       for (const err of checkResult.errors) {
-        console.error(`${err} in application/config/${section}.js`);
+        impress.console.error(`${err} in application/config/${section}.js`);
       }
       valid = false;
     }
   }
-  if (!valid) exit();
+  if (!valid) exit('Application server configuration is invalid');
+};
+
+const loadApplication = async (root) => {
+  impress.console.info(`Start: ${root}`);
+  const configPath = path.join(root, 'application/config');
+  const config = await new Config(configPath, CFG_OPTIONS).catch((err) => {
+    exit(`Can not read configuration: ${CFG_PATH}\n${err.stack}`);
+  });
+  await validateConfig(config);
+
+  const { balancer, ports = [], workers = {} } = config.server;
+  const threads = new Map();
+  const pool = new metautil.Pool({ timeout: workers.wait });
+
+  const app = { path: root, config, threads, pool, ready: 0 };
+
+  if (balancer) startWorker(app, 'balancer', balancer);
+  for (const port of ports) startWorker(app, 'server', port);
+  const poolSize = workers.pool || 0;
+  for (let i = 0; i < poolSize; i++) startWorker(app, 'worker');
+
+  impress.applications.set(root, app);
+};
+
+const loadApplications = async () => {
+  const list = await fsp
+    .readFile('.applications', 'utf8')
+    .then((data) => data.split('\n').filter((s) => s.length !== 0))
+    .catch(() => [PATH]);
+  for (const path of list) {
+    await loadApplication(path);
+  }
+};
+
+const stopApplication = (root) => {
+  const app = impress.applications.get(root);
+  for (const thread of app.threads.values()) {
+    thread.postMessage({ type: 'event', name: 'stop' });
+  }
+};
+
+const stop = async () => {
+  impress.finalization = true;
+  const logClosed = impress.logger.close();
+  const portsClosed = new Promise((resolve) => {
+    impress.finalized = resolve;
+    setTimeout(() => {
+      impress.console.error('Exit with graceful shutdown timeout');
+      resolve();
+    }, impress.config.server.timeouts.stop);
+  });
+  for (const app of impress.applications.values()) {
+    stopApplication(app.path);
+  }
+  await Promise.allSettled([logClosed, portsClosed]);
+  exit('Application server stopped');
 };
 
 (async () => {
-  const context = metavm.createContext({ process });
-  const CFG_OPTIONS = { mode: process.env.MODE, context };
-  const config = await new Config(CFG_PATH, CFG_OPTIONS).catch((err) => {
+  const configPath = path.join(PATH, 'application/config');
+  const config = await new Config(configPath, CFG_OPTIONS).catch((err) => {
     exit(`Can not read configuration: ${CFG_PATH}\n${err.stack}`);
   });
-
-  logger = await new Logger({ ...LOG_OPTIONS, ...config.log });
-  logger.on('error', logError('logger error'));
-  if (logger.active) global.console = logger.console;
-
   await validateConfig(config);
-  const { balancer, ports = [], workers = {} } = config.server;
-  const serversCount = ports.length + (balancer ? 1 : 0);
-  const schedulerCount = 1;
-  const schedulerId = serversCount;
-  const poolSize = workers.pool || 0;
-  const count = serversCount + schedulerCount + poolSize;
-  let startTimer = null;
-  let active = 0;
-  let starting = 0;
-  let scheduler = null;
-  const threads = new Array(count);
-  const pool = new metautil.Pool({ timeout: workers.wait });
-
-  const stop = async () => {
-    finalization = true;
-    const closing = logger.close();
-    for (const worker of threads) {
-      worker.postMessage({ type: 'event', name: 'stop' });
-    }
-    await closing;
-  };
-
-  const start = (id) => {
-    const workerPath = path.join(__dirname, 'lib/worker.js');
-    const worker = new Worker(workerPath, { trackUnmanagedFds: true });
-    threads[id] = worker;
-    if (id === schedulerId) scheduler = worker;
-    else if (id > schedulerId) pool.add(worker);
-
-    worker.on('exit', (code) => {
-      active--;
-      if (code !== 0) start(id);
-      else if (active === 0) process.exit(0);
-      else if (active < 0 && id === 0) exit('Application server stopped');
-    });
-
-    worker.on('online', () => {
-      if (++starting === count) {
-        startTimer = setTimeout(() => {
-          if (active !== count) {
-            console.warn(`Worker ${id} initialization timeout`);
-          }
-        }, config.server.timeouts.start);
-      }
-    });
-
-    const ITC = {
-      event: ({ name }) => {
-        if (name !== 'started') return;
-        active++;
-        if (active === count && startTimer) {
-          clearTimeout(startTimer);
-          startTimer = null;
-        }
-      },
-
-      task: (msg) => {
-        if (msg.type !== 'task') return;
-        const transferList = msg.port ? [msg.port] : undefined;
-        scheduler.postMessage(msg, transferList);
-      },
-
-      invoke: async (msg) => {
-        const { name, port, exclusive } = msg;
-        if (name === 'done') {
-          if (exclusive) pool.release(worker);
-          return;
-        }
-        if (name !== 'request') return;
-        const promisedThread = exclusive ? pool.capture() : pool.next();
-        const next = await promisedThread.catch(() => {
-          port.postMessage({ error: new Error('No thread available') });
-          return null;
-        });
-        if (!next) return;
-        next.postMessage(msg, [port]);
-      },
-    };
-
-    worker.on('message', (msg) => {
-      const handler = ITC[msg.type];
-      if (handler) handler(msg);
-    });
-  };
-
-  for (let id = 0; id < count; id++) start(id);
+  impress.config = config;
+  const logger = await new Logger({ ...LOG_OPTIONS, ...config.log });
+  logger.on('error', logError('Logger'));
+  if (logger.active) impress.console = logger.console;
+  impress.logger = logger;
 
   process.on('SIGINT', stop);
   process.on('SIGTERM', stop);
+
+  const startTimer = setTimeout(() => {
+    impress.console.warn(`Initialization timeout`);
+  }, config.server.timeouts.start);
+
+  await loadApplications();
 
   if (process.stdin.isTTY) {
     process.stdin.setRawMode(true);
@@ -164,5 +211,6 @@ const validateConfig = async (config) => {
       if (key === CTRL_C) stop();
     });
   }
-  initialization = false;
-})().catch(logError('initialization'));
+  clearTimeout(startTimer);
+  impress.initialization = false;
+})().catch(logError('Initialization'));
