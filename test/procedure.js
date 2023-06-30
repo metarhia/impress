ae4f578df35da2fe7b6a9d783d5f03cf024134b2
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-06-30 03:51:53 +0300

    Update tests due to metautil changes
    PR-URL: https://github.com/metarhia/impress/pull/1855

--- test/procedure.js
+++ test/procedure.js
@@ -93,7 +93,7 @@ metatests.testAsync('lib/procedure timeout', async (test) => {
 
   await test.rejects(
     async () => procedure.invoke({}, { waitTime: 150 }),
-    new Error('Timeout reached'),
+    new Error('Timeout of 100ms reached'),
   );
 
   await test.resolves(() => procedure.invoke({}, { waitTime: 50 }), DONE);
