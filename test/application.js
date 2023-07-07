3c5d4c691d750cf252fc6f231609cec934987cbf
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-07-08 01:59:10 +0300

    Add tests for `deps` and classes `Api`, `Application`

--- test/application.js
+++ test/application.js
@@ -1,12 +1,33 @@
 'use strict';
 
-const metatests = require('metatests');
+const { EventEmitter } = require('node:events');
 const wt = require('node:worker_threads');
+const metatests = require('metatests');
 const cwd = process.cwd();
 wt.workerData = { id: 0, kind: 'server', root: cwd, path: cwd, port: 8000 };
 const application = require('../lib/application.js');
 
 metatests.test('lib/application', (test) => {
-  test.strictSame(typeof application, 'object');
+  test.strictSame(application instanceof EventEmitter, true);
+  test.strictSame(application.constructor.name, 'Application');
+  test.strictSame(application.kind, 'server');
+  test.strictSame(application.initialization, true);
+  test.strictSame(application.finalization, false);
+  test.strictSame(typeof application.root, 'string');
+  test.strictSame(typeof application.path, 'string');
+  test.strictSame(application.schemas.constructor.name, 'Schemas');
+  test.strictSame(application.static.constructor.name, 'Static');
+  test.strictSame(application.cert.constructor.name, 'Cert');
+  test.strictSame(application.resources.constructor.name, 'Static');
+  test.strictSame(application.api.constructor.name, 'Api');
+  test.strictSame(application.lib.constructor.name, 'Place');
+  test.strictSame(application.db.constructor.name, 'Place');
+  test.strictSame(application.bus.constructor.name, 'Place');
+  test.strictSame(application.starts, []);
+  test.strictSame(application.config, null);
+  test.strictSame(application.logger, null);
+  test.strictSame(application.console, null);
+  test.strictSame(application.auth, null);
+  test.strictSame(application.watcher, null);
   test.end();
 });
--- test/deps.js
+++ test/deps.js
@@ -1,11 +1,18 @@
 'use strict';
 
 const metatests = require('metatests');
-const { node, npm, metarhia } = require('../lib/deps.js');
+const deps = require('../lib/deps.js');
+const { node, npm, metarhia } = deps;
 
 metatests.test('lib/deps', async (test) => {
+  const expectedNamespaces = ['node', 'npm', 'metarhia', 'notLoaded', 'wt'];
+  test.strictSame(Object.keys(deps), expectedNamespaces);
+  test.strictSame(typeof node, 'object');
   test.strictSame(typeof node.os, 'object');
+  test.strictSame(typeof npm, 'object');
   test.strictSame(typeof npm.ws, 'function');
+  test.strictSame(typeof metarhia, 'object');
   test.strictSame(typeof metarhia.metaschema, 'object');
+  test.strictSame(deps.notLoaded instanceof Set, true);
   test.end();
 });
