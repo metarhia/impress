743a34c903a2b3b43cd33c28b0f940c8ed35fd10
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-07-08 23:58:10 +0300

    More tests for `Procedure`

--- test/procedure.js
+++ test/procedure.js
@@ -4,6 +4,49 @@ const metatests = require('metatests');
 const metautil = require('metautil');
 const { Procedure } = require('../lib/procedure.js');
 
+metatests.testAsync('lib/procedure', async (test) => {
+  const script = () => ({
+    method: async ({ a, b }) => a + b,
+  });
+
+  const application = {
+    server: {
+      semaphore: {
+        async enter() {},
+        leave() {},
+      },
+    },
+  };
+
+  const procedure = new Procedure(script, 'method', application);
+
+  test.strictSame(procedure.constructor.name, 'Procedure');
+  test.strictSame(typeof procedure.exports, 'object');
+  test.strictSame(typeof procedure.exports.method, 'function');
+  test.strictSame(typeof procedure.script, 'function');
+  test.strictSame(procedure.methodName, 'method');
+  test.strictSame(typeof procedure.application, 'object');
+  test.strictSame(typeof procedure.method, 'function');
+  test.strictSame(procedure.method.constructor.name, 'AsyncFunction');
+  test.strictSame(procedure.parameters, null);
+  test.strictSame(procedure.returns, null);
+  test.strictSame(procedure.errors, null);
+  test.strictSame(procedure.semaphore, null);
+  test.strictSame(procedure.caption, '');
+  test.strictSame(procedure.description, '');
+  test.strictSame(procedure.access, '');
+  test.strictSame(procedure.validate, null);
+  test.strictSame(typeof procedure.timeout, 'number');
+  test.strictSame(procedure.serializer, null);
+  test.strictSame(procedure.protocols, null);
+  test.strictSame(procedure.deprecated, false);
+  test.strictSame(procedure.assert, null);
+  test.strictSame(procedure.examples, null);
+
+  await test.resolves(() => procedure.invoke({}, { a: 4, b: 6 }), 10);
+  test.end();
+});
+
 metatests.testAsync('lib/procedure validate', async (test) => {
   const script = () => ({
     validate: ({ a, b }) => {
