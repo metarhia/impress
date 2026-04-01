04bac0379900549d1cc20d69c2a21c88b969f310
Turone <turone@ukr.net>
2026-04-01 14:36:31 +0300

    optimize cache and static file handling after testing with SAB

--- schemas/config/cache.js
+++ schemas/config/cache.js
@@ -2,5 +2,6 @@
   size: 'size',
   maxFileSize: 'size',
   streamThreshold: { type: 'size', required: false },
+  virtualFS: { type: 'boolean', required: false },
   avoid: { array: 'string', required: false },
 });
